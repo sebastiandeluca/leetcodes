f340b6cab335fa851f13b8166d87a0f0f969cc0f
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2025-01-25 15:43:08 -0500

    2130

---
+++ 2130.py
@@ -0,0 +1,31 @@
+# Maximum Sum of a Linked List
+# Soln: 194ms runtime, beats 57.35%, 71.32MB memory, beats 66.97%
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def pairSum(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: int
+        """
+        s, f = head, head
+        value = 0
+
+        while f and f.next:
+            f = f.next.next
+            s = s.next
+        
+        curr, prev = s, None
+
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+
+        while prev:
+            value = max(value, head.val + prev.val)
+            prev = prev.next
+            head = head.next
+        return value
