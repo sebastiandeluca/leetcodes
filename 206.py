9924db02338071b8de9139df54594799bad77f33
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2025-01-25 15:38:18 -0500

    206, recursion edition

--- 206.py
+++ 206.py
@@ -1,5 +1,25 @@
 # Reverse a Linked List
-#Soln: 3ms runtime, beats 16.68%, 14.41mb memory, beats 41.40% (attempt 1)
+#Soln: 3ms runtime, beats 16.68%, 14.41mb memory, beats 41.40% (attempt 2, recursion)
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not head or not head.next:
+            return head
+
+        reversed_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return reversed_head
+        
 
 # Definition for singly-linked list.
 # class ListNode(object):
