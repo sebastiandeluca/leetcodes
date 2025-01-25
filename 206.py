dbcc1c4cf664e3253c87f5eb5e996bd9618726cd
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2025-01-25 15:31:57 -0500

    206

---
+++ 206.py
@@ -0,0 +1,29 @@
+# Reverse a Linked List
+#Soln: 3ms runtime, beats 16.68%, 14.41mb memory, beats 41.40% (attempt 1)
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        nodes = []
+        n = head
+        while n:
+            nodes.append(n)
+            n = n.next
+        if not nodes or len(nodes) == 1:
+            return head
+        new_head = nodes[-1]
+        n2 = new_head
+        for x in range(len(nodes) - 2, -1, -1):
+            n2.next = nodes[x]
+            n2 = n2.next
+        n2.next = None
+        return new_head
+        
