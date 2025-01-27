babe7d7399d69bc094d37c976f66d355e205998e
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2025-01-27 09:34:19 -0500

    345-2

--- 345.py
+++ 345.py
@@ -1,4 +1,26 @@
 # Reverse Vowels of a String\
+# Soln: 19ms Runtime, beats 69.94%, 13.86MB memory, beats 38.58%
+class Solution(object):
+    def reverseVowels(self, s):
+        word = list(s)
+        start = 0
+        end = len(s) - 1
+        vowels = "aeiouAEIOU"
+        
+        while start < end:
+            while start < end and vowels.find(word[start]) == -1:
+                start += 1
+            
+            while start < end and vowels.find(word[end]) == -1:
+                end -= 1
+            
+            word[start], word[end] = word[end], word[start]
+            start += 1
+            end -= 1
+        
+        return "".join(word)
+
+
 # Soln: 324ms Runtime, beats 18.05%, 17.11MB memory, beats 9.06% (Attempt 1)
 
 class Solution(object):
