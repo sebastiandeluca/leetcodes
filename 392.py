fad616f211e70bcc7407d385d05d9a871871cb52
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2025-01-28 11:10:26 -0500

    392

---
+++ 392.py
@@ -0,0 +1,25 @@
+# Is Subsequence
+# Soln: 330ms Runtime, beats 5.60%, 13.06MB memory, beats 9.39% (Attempt 1)
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        l = 0
+        n = len(t)
+        t = list(t)
+        b = False
+        for x in range(n):
+            if l < len(s) and t[x] == s[l]:
+                t[x], t[l] = t[l], t[x]
+                print(s[l],l,t)
+                l += 1
+        if l < len(s):
+            b = False # Case where s in t but not as a subsequence
+        else:
+            b = s in "".join(t) # Normal result
+        return b
+        
