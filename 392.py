a6809330316ae9c5d08fe415a7e3e12036efc7b9
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2025-01-28 11:13:02 -0500

    392-2

--- 392.py
+++ 392.py
@@ -1,4 +1,21 @@
 # Is Subsequence
+# Soln: 0ms Runtime, beats 100.00%, 12.86MB memory, beats 13.91%
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        l = 0
+        n = len(t)
+        for x in range(n):
+            if l < len(s) and t[x] == s[l]:
+                l += 1
+        return l >= len(s)
+    # Realized we don't return the subsequence, no point in swapping things!
+
+
 # Soln: 330ms Runtime, beats 5.60%, 13.06MB memory, beats 9.39% (Attempt 1)
 
 class Solution(object):
