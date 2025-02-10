33c68bb0cda8be33714f09444c23e6c5cd556f10
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2025-02-10 18:30:46 -0500

    49

---
+++ 49.py
@@ -0,0 +1,13 @@
+# Group Anagrams
+# beats like 90% i dont care about the stats anymore
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return list(ans.values())
